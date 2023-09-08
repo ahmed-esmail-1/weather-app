56751895f8c5a33099053dc3160dd8921958a0f3
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-09-09 01:17:18 +0300

    Adding City, fixing styles, city, degree

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div id="app">
     <main>
+      
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search.." />
         
       </div>
+      
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Jeddah, Saudi Arabia</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">50°c</div>
+          <div class="weather">Snow</div>
+        </div>
+      </div>
+
     </main>
   </div>
 </template>
@@ -22,7 +35,7 @@ export default {
 </script>
 
 <style>
-*{
+* {
   margin:0;
   padding:0;
   box-sizing: border-box;
@@ -32,7 +45,7 @@ body {
   font-family: 'montserrat', sans-serif;
 }
 
-#app{
+#app {
   background-image: url('./assets/cold-bg.jpg');
   background-size: cover;
   background-position: bottom;
@@ -46,7 +59,7 @@ main {
   background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
 }
 
-.search-box{
+.search-box {
   width: 100%;
   margin-bottom: 30px;
 }
@@ -64,8 +77,46 @@ main {
   outline: none;
   background: none;
 
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);;
   background-color: rgba(255,255,255,0.5);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.25);
 }
 </style>
